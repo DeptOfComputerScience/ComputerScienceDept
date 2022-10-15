b02d3ac582edeb5dfd455376956146c28c86d34a
MANASH CHOUDHURI <manashchoudhuri4@gmail.com>
2022-10-15 21:35:01 +0530

    changing the structure of navbar dropdown content

--- src/components/nabvar.jsx
+++ src/components/nabvar.jsx
@@ -1,18 +1,14 @@
 import { Fragment } from 'react'
-import { Link } from "react-router-dom";
 import { Popover, Transition } from '@headlessui/react'
 import {
-  ArrowPathIcon,
   Bars3Icon,
   BookmarkSquareIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  CursorArrowRaysIcon,
+  BellSnoozeIcon,
+  BellAlertIcon,
+  UserGroupIcon,
+  UserCircleIcon,
   LifebuoyIcon,
-  PhoneIcon,
-  PlayIcon,
-  ShieldCheckIcon,
-  Squares2X2Icon,
+  AcademicCapIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
@@ -29,69 +25,42 @@ const page = [
 const academics = [
   {
     name: 'Faculty',
-    description: 'Get a better understanding of where your traffic is coming from.',
     href: page[2]['route'],
-    icon: ChartBarIcon,
+    icon: UserGroupIcon,
   },
   {
     name: 'Students',
-    description: 'Speak directly to your customers in a more meaningful way.',
     href: page[3]['route'],
-    icon: CursorArrowRaysIcon,
+    icon: UserCircleIcon,
   },
   { name: 'Alumni', 
-    description: "Your customers' data will be safe and secure.",
     href: page[0]['route'], 
-    icon: ShieldCheckIcon },
-  {
-    name: 'Integrations',
-    description: "Connect with third-party tools that you're already using.",
-    href: '#',
-    icon: Squares2X2Icon,
-  },
-  {
-    name: '___',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch Demo', href: '#', icon: PlayIcon },
-  { name: 'Contact Sales', href: '#', icon: PhoneIcon },
+    icon: AcademicCapIcon, },
 ]
 const events = [
   {name: 'Past events',
-  description: 'See what meet-ups and other events we might be planning near you.',
   href: page[1]['route'],
-  icon: CalendarIcon,
+  icon: BellSnoozeIcon,
   },
   {
     name: 'Upcoming events',
-    description: 'Get all of your questions answered in our forums or contact support.',
     href: page[1]['route'],
-    icon: CalendarIcon,
+    icon: BellAlertIcon,
   },
 ]
 const resources = [
   {
     name: 'Help Center',
     description: 'Get all of your questions answered in our forums or contact support.',
-    href: '#',
+    href: '/',
     icon: LifebuoyIcon,
   },
   {
     name: 'Guides',
     description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '#',
+    href: '/',
     icon: BookmarkSquareIcon,
   },
-  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
-]
-const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
 ]
 
 function classNames(...classes) {
@@ -102,7 +71,7 @@ export default function Example() {
   return (
     <Popover className="relative bg-white">
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
-        <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div className="flex items-center justify-between border-b-2 border-gray-100 py-4 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href='/'>
               <span className="sr-only">CS Department</span>
@@ -123,6 +92,7 @@ export default function Example() {
             <Popover className="relative">
               {({ open }) => (
                 <>
+                  
                   <Popover.Button
                     className={classNames(
                       open ? 'text-gray-900' : 'text-gray-500',
@@ -163,19 +133,6 @@ export default function Example() {
                                 <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                               </div>
                             </a>
-                          ))}
-                        </div>
-                        <div className="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100"
-                              >
-                                <item.icon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
                           ))}
                         </div>
                       </div>
@@ -289,26 +246,6 @@ export default function Example() {
                             </a>
                           ))}
                         </div>
-                        <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
-                          <div>
-                            <h3 className="text-base font-medium text-gray-500">Recent Posts</h3>
-                            <ul role="list" className="mt-4 space-y-4">
-                              {recentPosts.map((post) => (
-                                <li key={post.id} className="truncate text-base">
-                                  <a href={post.href} className="font-medium text-gray-900 hover:text-gray-700">
-                                    {post.name}
-                                  </a>
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                          <div className="mt-5 text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                              View all posts
-                              <span aria-hidden="true"> &rarr;</span>
-                            </a>
-                          </div>
-                        </div>
                       </div>
                     </Popover.Panel>
                   </Transition>
@@ -317,11 +254,11 @@ export default function Example() {
             </Popover>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
               Sign in
             </a>
             <a
-              href="#"
+              href="/"
               className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             >
               Sign up
@@ -346,8 +283,8 @@ export default function Example() {
                 <div>
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company"
+                    src="GU.png"
+                    alt="..."
                   />
                 </div>
                 <div className="-mr-2">
@@ -402,14 +339,14 @@ export default function Example() {
               </div>
               <div>
                 <a
-                  href="#"
+                  href="/"
                   className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                 >
                   Sign up
                 </a>
                 <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                  Existing student/faculty?{' '}
+                  <a href="/" className="text-indigo-600 hover:text-indigo-500">
                     Sign in
                   </a>
                 </p>
